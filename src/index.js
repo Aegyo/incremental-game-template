374cf4aad495a6c692eef7d79f5a88d8f763a7c2
Isha Dijcks <ishadijcks@gmail.com>
2020-08-04 18:49:28 +0200

    Split build modes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import printMe from './print';
+
 function component() {
     const element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+    const btn = document.createElement('button');
+
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
     return element;
 }
 
