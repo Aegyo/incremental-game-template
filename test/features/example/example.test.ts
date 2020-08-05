087709a512f1e0479ecf8cc4934242583cf5a198
Isha Dijcks <ishadijcks@gmail.com>
2020-08-05 18:58:56 +0200

    Fix failing test

--- test/features/example/example.test.ts
+++ test/features/example/example.test.ts
@@ -3,7 +3,3 @@ import Example from "../../../src/features/example/example";
 test('gets three', () => {
     expect(Example.getThree()).toBe(3);
 });
-
-test('I will fail', () => {
-    expect(2).toBe(3);
-});
