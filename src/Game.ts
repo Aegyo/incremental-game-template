d8ebd1974da42db630418798456f9604ea54d84c
Isha Dijcks <ishadijcks@gmail.com>
2020-08-05 22:41:43 +0200

    Add game classes

---
+++ src/Game.ts
@@ -0,0 +1,41 @@
+import {Example} from "./features/example/example";
+import {GameState} from "./GameState";
+
+export class Game {
+    private _tickInterval: any;
+
+    public example: Example;
+
+    private readonly _state: KnockoutObservable<GameState>;
+
+    private readonly TICK_DURATION_MS = 50;
+
+    constructor(example: Example) {
+        this.example = example;
+        this._state = ko.observable(GameState.starting);
+    }
+
+    private update(): void {
+
+    }
+
+    public initialize(): void {
+
+    }
+
+    public start(): void {
+        this._tickInterval = setInterval(() => this.update(), this.TICK_DURATION_MS);
+        this.state = GameState.playing;
+        console.log("Started");
+    }
+
+
+    // Knockout getters/setters
+    get state(): GameState {
+        return this._state();
+    }
+
+    set state(value: GameState) {
+        this._state(value);
+    }
+}
---
+++ src/GameState.ts
@@ -0,0 +1,10 @@
+/**
+ * starting: The default, game is still loading
+ * playing: Normal gameplay
+ * paused: Gameplay is suspended
+ */
+export enum GameState {
+    starting,
+    playing,
+    paused
+}
